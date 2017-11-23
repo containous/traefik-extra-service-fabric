7d7070406a41ed0cb3532de0ed6be5159e44d309
Fernandez Ludovic <ludovic@containo.us>
2017-11-24 00:13:35 +0100

    chore: use new client version.

--- client_mock_test.go
+++ client_mock_test.go
@@ -1,45 +1,42 @@
 package servicefabric
 
 import (
-	sfsdk "github.com/jjcollinge/servicefabric"
+	sf "github.com/jjcollinge/servicefabric"
 )
 
 type clientMock struct {
-	applications *sfsdk.ApplicationItemsPage
-	services     *sfsdk.ServiceItemsPage
-	partitions   *sfsdk.PartitionItemsPage
-	replicas     *sfsdk.ReplicaItemsPage
-	instances    *sfsdk.InstanceItemsPage
+	applications *sf.ApplicationItemsPage
+	services     *sf.ServiceItemsPage
+	partitions   *sf.PartitionItemsPage
+	replicas     *sf.ReplicaItemsPage
+	instances    *sf.InstanceItemsPage
 	labels       map[string]string
 }
 
-func (c *clientMock) GetApplications() (*sfsdk.ApplicationItemsPage, error) {
+func (c *clientMock) GetApplications() (*sf.ApplicationItemsPage, error) {
 	return c.applications, nil
 }
 
-func (c *clientMock) GetServices(appName string) (*sfsdk.ServiceItemsPage, error) {
+func (c *clientMock) GetServices(appName string) (*sf.ServiceItemsPage, error) {
 	return c.services, nil
 }
 
-func (c *clientMock) GetPartitions(appName, serviceName string) (*sfsdk.PartitionItemsPage, error) {
+func (c *clientMock) GetPartitions(appName, serviceName string) (*sf.PartitionItemsPage, error) {
 	return c.partitions, nil
 }
 
-func (c *clientMock) GetReplicas(appName, serviceName, partitionName string) (*sfsdk.ReplicaItemsPage, error) {
+func (c *clientMock) GetReplicas(appName, serviceName, partitionName string) (*sf.ReplicaItemsPage, error) {
 	return c.replicas, nil
 }
 
-func (c *clientMock) GetInstances(appName, serviceName, partitionName string) (*sfsdk.InstanceItemsPage, error) {
+func (c *clientMock) GetInstances(appName, serviceName, partitionName string) (*sf.InstanceItemsPage, error) {
 	return c.instances, nil
 }
 
 func (c *clientMock) GetServiceExtension(appType, applicationVersion, serviceTypeName, extensionKey string, response interface{}) error {
 	return nil
 }
-func (c *clientMock) GetProperties(name string) (bool, map[string]string, error) {
-	return true, c.labels, nil
-}
 
-func (c *clientMock) GetServiceLabels(service *sfsdk.ServiceItem, app *sfsdk.ApplicationItem, prefix string) (map[string]string, error) {
+func (c *clientMock) GetServiceLabels(service *sf.ServiceItem, app *sf.ApplicationItem, prefix string) (map[string]string, error) {
 	return c.labels, nil
 }
