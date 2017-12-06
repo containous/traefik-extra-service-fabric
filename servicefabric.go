0e2c286afab7a99fe2deb14451dbd6e62cea6f28
Fernandez Ludovic <ludovic@containo.us>
2017-12-07 00:00:01 +0100

    refactor(label): use new Traefik methods.

--- servicefabric.go
+++ servicefabric.go
@@ -77,22 +77,21 @@ func (p *Provider) updateConfig(configurationChan chan<- types.ConfigMessage, po
 				templateObjects := struct {
 					Services []ServiceItemExtended
 				}{
-					services,
+					Services: services,
 				}
 
 				var sfFuncMap = template.FuncMap{
-					"isPrimary":                       isPrimary,
-					"getDefaultEndpoint":              p.getDefaultEndpoint,
-					"getNamedEndpoint":                p.getNamedEndpoint,
-					"getApplicationParameter":         p.getApplicationParameter,
-					"doesAppParamContain":             p.doesAppParamContain,
-					"hasServiceLabel":                 hasServiceLabel,
-					"getServiceLabelValue":            getServiceLabelValue,
-					"getServiceLabelValueWithDefault": getServiceLabelValueWithDefault,
-					"getServiceLabelsWithPrefix":      getServiceLabelsWithPrefix,
-					"getServicesWithLabelValueMap":    getServicesWithLabelValueMap,
-					"getServicesWithLabelValue":       getServicesWithLabelValue,
-					"isExposed":                       getFuncBoolLabel("expose"),
+					"isPrimary":                    isPrimary,
+					"getDefaultEndpoint":           p.getDefaultEndpoint,
+					"getNamedEndpoint":             p.getNamedEndpoint,
+					"getApplicationParameter":      p.getApplicationParameter,
+					"doesAppParamContain":          p.doesAppParamContain,
+					"hasLabel":                     hasFunc(),
+					"getLabelValue":                getFuncStringLabel(""),
+					"getLabelValueWithDefault":     getFuncStringLabelWithDefault(),
+					"getLabelsWithPrefix":          getLabelsWithPrefix,
+					"getServicesWithLabelValueMap": getServicesWithLabelValueMap,
+					"isExposed":                    getFuncBoolLabel("expose", false),
 				}
 
 				configuration, err := p.GetConfiguration(tmpl, sfFuncMap, templateObjects)
@@ -250,27 +249,7 @@ func getServicesWithLabelValueMap(services []ServiceItemExtended, key string) ma
 	return result
 }
 
-func getServicesWithLabelValue(services []ServiceItemExtended, key, expectedValue string) []ServiceItemExtended {
-	var srvWithLabel []ServiceItemExtended
-	for _, service := range services {
-		value, exists := service.Labels[key]
-		if exists && value == expectedValue {
-			srvWithLabel = append(srvWithLabel, service)
-		}
-	}
-	return srvWithLabel
-}
-
-func getServiceLabelValueWithDefault(service ServiceItemExtended, key, defaultValue string) string {
-	value, exists := service.Labels[key]
-
-	if !exists {
-		return defaultValue
-	}
-	return value
-}
-
-func getServiceLabelsWithPrefix(service ServiceItemExtended, prefix string) map[string]string {
+func getLabelsWithPrefix(service ServiceItemExtended, prefix string) map[string]string {
 	results := make(map[string]string)
 	for k, v := range service.Labels {
 		if strings.HasPrefix(k, prefix) {
