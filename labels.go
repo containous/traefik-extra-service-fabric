b0341b88f2fd389a8d5852e7b234fb7da36bc4f5
Fernandez Ludovic <ludovic@containo.us>
2017-12-09 00:56:17 +0100

    refactor: use new labels methods from Traefik.

--- labels.go
+++ labels.go
@@ -1,63 +1,50 @@
 package servicefabric
 
 import (
-	"strconv"
+	"github.com/containous/traefik/provider/label"
 )
 
 func getFuncBoolLabel(labelName string, defaultValue bool) func(service ServiceItemExtended) bool {
 	return func(service ServiceItemExtended) bool {
-		return getBoolValue(service.Labels, labelName, defaultValue)
+		return label.GetBoolValue(service.Labels, labelName, defaultValue)
 	}
 }
 
-// ------
-
+// Must be replaced by:
+// func getFuncStringLabel(labelName string, defaultValue string) func(service ServiceItemExtended) string {
+// 	return func(service ServiceItemExtended) string {
+// 		return label.GetStringValue(service.Labels, labelName, defaultValue)
+// 	}
+// }
 // Deprecated
 func getFuncStringLabelWithDefault() func(service ServiceItemExtended, labelName string, defaultValue string) string {
 	return func(service ServiceItemExtended, labelName string, defaultValue string) string {
-		return getStringValue(service.Labels, labelName, defaultValue)
+		return label.GetStringValue(service.Labels, labelName, defaultValue)
 	}
 }
 
+// Must be replaced by:
+// func hasFunc(labelName string) func(service ServiceItemExtended) bool {
+// 	return func(service ServiceItemExtended) bool {
+// 		return label.Has(service.Labels, labelName)
+// 	}
+// }
 // Deprecated
 func hasFunc() func(service ServiceItemExtended, labelName string) bool {
 	return func(service ServiceItemExtended, labelName string) bool {
-		return hasLabelNew(service.Labels, labelName)
+		return label.Has(service.Labels, labelName)
 	}
 }
 
+// Must be replaced by:
+// func getFuncStringLabel(labelName string, defaultValue string) func(service ServiceItemExtended) string {
+// 	return func(service ServiceItemExtended) string {
+// 		return label.GetStringValue(service.Labels, labelName, defaultValue)
+// 	}
+// }
 // Deprecated
 func getFuncStringLabel(defaultValue string) func(service ServiceItemExtended, labelName string) string {
 	return func(service ServiceItemExtended, labelName string) string {
-		return getStringValue(service.Labels, labelName, defaultValue)
+		return label.GetStringValue(service.Labels, labelName, defaultValue)
 	}
-}
-
-// must be replace by label.Has()
-// Deprecated
-func hasLabelNew(labels map[string]string, labelName string) bool {
-	value, ok := labels[labelName]
-	return ok && len(value) > 0
-}
-
-// must be replace by label.GetStringValue()
-// Deprecated
-func getStringValue(labels map[string]string, labelName string, defaultValue string) string {
-	if value, ok := labels[labelName]; ok && len(value) > 0 {
-		return value
-	}
-	return defaultValue
-}
-
-// must be replace by label.GetBoolValue()
-// Deprecated
-func getBoolValue(labels map[string]string, labelName string, defaultValue bool) bool {
-	rawValue, ok := labels[labelName]
-	if ok {
-		v, err := strconv.ParseBool(rawValue)
-		if err == nil {
-			return v
-		}
-	}
-	return defaultValue
 }
