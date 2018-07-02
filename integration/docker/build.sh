eb0ac4df5d5be1f0358afabcdf6e12cdbb7c368c
lawrencegripper <info@grippers.co.uk>
2018-07-02 18:37:00 +0100

    Fix directory location  for docker build

--- integration/docker/build.sh
+++ integration/docker/build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
 DOCKERLOCATION="lawrencegripper"
-DOCKERVERSION="0.1"
+DOCKERVERSION="0.2"
 
 echo "######## Building docker images ###########"
 docker build -t $DOCKERLOCATION/sfonebox:$DOCKERVERSION -t $DOCKERLOCATION/sfonebox:latest -f ./cluster.Dockerfile .
 docker build -t $DOCKERLOCATION/sfoneboxwithnode:$DOCKERVERSION -t $DOCKERLOCATION/sfoneboxwithnode:latest -f ./clusterwithnode.Dockerfile .
 docker build -t $DOCKERLOCATION/sfctl:$DOCKERVERSION -t $DOCKERLOCATION/sfctl:latest -f ./sfctl.Dockerfile .
 
-docker push $DOCKERLOCATION/sfonebox
-docker push $DOCKERLOCATION/sfoneboxwithnode
-docker push $DOCKERLOCATION/sfctl
+
 
